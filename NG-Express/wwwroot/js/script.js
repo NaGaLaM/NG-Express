a407ced9555ee5c22cb4acdae3134fa60120117c
Narek Galstyan <narekgalstyanjs@gmail.com>
2024-06-23 16:48:49 +0400

    [UI] add to card button created

--- NG-Express/wwwroot/js/script.js
+++ NG-Express/wwwroot/js/script.js
@@ -4,12 +4,12 @@
     let marginleft = parseInt(window.getComputedStyle(cardBox).marginLeft);
     if (marginleft > -2310)
     {
-        if (2310 + marginleft < 770)
+        if (2310 + marginleft < 800)
         {
             marginleft = -2310;
         } else
         {
-        marginleft -= 770;
+        marginleft -= 800;
         }
         cardBox.style.marginLeft = `${marginleft}px`
     }
@@ -18,12 +18,12 @@ function prev() {
     let cardBox = document.querySelector(".card-box");
     let marginleft = parseInt(window.getComputedStyle(cardBox).marginLeft);
     if (marginleft < 2310) {
-        if (0-marginleft < 770) {
+        if (0-marginleft < 800) {
             marginleft = 0;
         }
         else
         {
-        marginleft += 770;
+        marginleft += 800;
         }
         cardBox.style.marginLeft = `${marginleft}px`
     }
