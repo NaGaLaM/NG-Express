f640eb0006beafaf3e4896a32c67737e31c0014a
Narek Galstyan <narekgalstyanjs@gmail.com>
2024-06-23 15:53:55 +0400

    [back-end] products slider in home page,wishlist and favorite button fix

---
+++ NG-Express/wwwroot/js/script.js
@@ -0,0 +1,30 @@
+﻿function next()
+{
+    let cardBox = document.querySelector(".card-box");
+    let marginleft = parseInt(window.getComputedStyle(cardBox).marginLeft);
+    if (marginleft > -2310)
+    {
+        if (2310 + marginleft < 770)
+        {
+            marginleft = -2310;
+        } else
+        {
+        marginleft -= 770;
+        }
+        cardBox.style.marginLeft = `${marginleft}px`
+    }
+}
+function prev() {
+    let cardBox = document.querySelector(".card-box");
+    let marginleft = parseInt(window.getComputedStyle(cardBox).marginLeft);
+    if (marginleft < 2310) {
+        if (0-marginleft < 770) {
+            marginleft = 0;
+        }
+        else
+        {
+        marginleft += 770;
+        }
+        cardBox.style.marginLeft = `${marginleft}px`
+    }
+}
