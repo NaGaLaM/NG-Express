ff9815038214c8b40a57740eb308c4bc5b25ba43
Narek Galstyan <narekgalstyanjs@gmail.com>
2024-07-21 01:45:19 +0400

    [Fix] Authorization FIxed added @rendermode@(new InteractiveServerRenderMode(prerender:false)) in Router, adding CascadingRouterView, adding policy for unauthorized users

--- NG-Express/Security/AuthToken.cs
+++ NG-Express/Security/AuthToken.cs
@@ -9,15 +9,20 @@ namespace NG_Express.Security
     public class AuthToken
     {
         private readonly IConfiguration _configuration;
+        private readonly string _issuer;
+        private readonly string _audience;
+        private readonly string SecretKey;
         public AuthToken(IConfiguration config)
         {
-            _configuration = config;
+            _issuer = "Issuer";
+            _audience = "Audience";
+            SecretKey = config.GetValue<string>("JWT:SecretKey") ?? string.Empty;
 
         }
         public string GenerateToken(Buyer buyer)
         {
-            var SecretKey = Encoding.UTF8.GetBytes(_configuration.GetValue<string>("JWT:SecretKey"));
-            var symmetricSecurirtKey = new SymmetricSecurityKey(SecretKey);
+            var Key = Encoding.UTF8.GetBytes(SecretKey);
+            var symmetricSecurirtKey = new SymmetricSecurityKey(Key);
             var signingCredentials = new SigningCredentials(symmetricSecurirtKey, SecurityAlgorithms.HmacSha256);
 
             var claims = new[]
@@ -26,14 +31,42 @@ namespace NG_Express.Security
                 new Claim("Name",buyer.FirstName),
             };
             var tokenSchema = new JwtSecurityToken(
-                issuer: "Issuer",
-                audience: "Audience",
+                issuer: _issuer,
+                audience: _audience,
                 claims: claims,
-                expires: DateTime.UtcNow.AddDays(1),
+                expires: DateTime.UtcNow.AddSeconds(10),
                 signingCredentials: signingCredentials
                 );
 
             return new JwtSecurityTokenHandler().WriteToken(tokenSchema);
+        }
+        public ClaimsPrincipal ValidateToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(SecretKey);
+
+            try
+            {
+                var validationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = _issuer,
+                    ValidAudience = _audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ClockSkew = TimeSpan.FromSeconds(10)
+                };
+
+                var principal = tokenHandler.ValidateToken(token, validationParameters, out var validatedToken);
+                return principal;
+            }
+            catch 
+            {
+                Console.WriteLine($"Token Isnt Valid.");
+                return null;
+            }
         }
     }
 }
